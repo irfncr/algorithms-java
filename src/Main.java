e59a6c6992dbcfa61d64c7c10b02df8fbedbf798
Irfan <irfncr@gmail.com>
2020-01-25 17:51:04 -0600

    uf api is added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        int N = 0;
+        Scanner input = new Scanner(System.in);
+        N = input.nextInt();
+        UF uf = new UF(N);
+        while (input.hasNextInt())
+        {
+            int p = input.nextInt();
+            int q = input.nextInt();
+            if(!uf.connected(p,q)) {
+                uf.union(p,q);
+                System.out.println(p + " " + q + " is connected now.");
+            }else{
+                System.out.println(p + " " + q + " is connected");
+            }
+        }
         System.out.println("Hello World!");
     }
 }
---
+++ src/UF.java
@@ -0,0 +1,14 @@
+public class UF {
+    public UF(int N)
+    {
+
+    }
+    public boolean connected(int p, int q){
+
+        return true;
+    }
+
+    public void union(int p, int q){
+
+    }
+}
