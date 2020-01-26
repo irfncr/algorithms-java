af8ee748680c522b349176b10d1d1c56e86ac337
Irfan <irfncr@gmail.com>
2020-01-25 18:19:16 -0600

    quick find is added

--- src/UF.java
+++ src/UF.java
@@ -1,14 +1,23 @@
 public class UF {
+
+    private int[] quickFindArray;
     public UF(int N)
     {
-
+        for(int i = 0; i < N; i++){
+            quickFindArray[i] = i;
+        }
     }
     public boolean connected(int p, int q){
 
-        return true;
+        return quickFindArray[p] == quickFindArray[q];
     }
 
     public void union(int p, int q){
-
+        int temp = quickFindArray[p];
+        for (int i = 0; i < quickFindArray.length; i++) {
+            if(quickFindArray[i] == temp){
+                quickFindArray[i] = quickFindArray[q];
+            }
+        }
     }
 }
