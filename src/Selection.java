a23afb02f9a426f493a1b08b21feef10f37b4a6f
Irfan <irfncr@gmail.com>
2020-02-11 17:26:08 -0600

    Selection sort is added

--- src/Selection.java
+++ src/Selection.java
@@ -1,4 +1,18 @@
 public class Selection {
+    public static void sort(Comparable[] a){
+
+        int N = a.length;
+
+        for (int i = 0; i < a.length; i++){
+            int min = i;
+            for(int j = i+1; i < a.length; j++){
+                if(less(a[j], a[i])){
+                    min = j;
+                }
+            }
+            exch(a, i, min);
+        }
+    }
     public static void exch(Comparable[] a, int i, int j){
         Comparable temp = a[i];
         a[i] = a[j];
